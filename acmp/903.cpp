a92a5b9c0ac49e9e1993c2b6c6b7751ed67fc14a
Andrey Kuznetsov <t2uwsmd@gmail.com>
2024-09-04 19:21:31 +0400

    903 acmp

---
+++ acmp/903.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+
+#define ll long long
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    cout << n+1;
+    return 0;
+}
